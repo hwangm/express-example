0613823ecdeb5c69522c46e558c9ca5fc583d72f
Matthew Hwang <hw4ng_m@yahoo.com>
2018-02-12 21:54:15 -0800

    saving progress

--- models/author.js
+++ models/author.js
@@ -1,26 +1,37 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
     {
-        first_name: {type: String, required: true, max: 100},
-        family_name: {type: String, required: true, max: 100},
-        date_of_birth: {type: Date},
-        date_of_death: {type: Date}
+      first_name: {type: String, required: true, max: 100},
+      family_name: {type: String, required: true, max: 100},
+      date_of_birth: {type: Date},
+      date_of_death: {type: Date},
     }
-);
+  );
 
+// Virtual for author's full name
 AuthorSchema
 .virtual('name')
-.get(() => {
-    return this.family_name + ', '+ this.first_name;
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
 });
 
 AuthorSchema
-.virtual('url')
+.virtual('lifespan')
 .get(() => {
-    return '/catalog/author/' + this._id;
+    let birth = this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+    let death = this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+    return birth + ' - ' + death;
 });
 
 module.exports = mongoose.model('Author', AuthorSchema);
